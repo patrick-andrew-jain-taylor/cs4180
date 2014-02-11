ac781a121df5fc0c433410baa8b35778d46406a9
Patrick Taylor <trickyconverse@gmail.com>
2014-02-10 21:20:42 -0500

    Updated client1/2 and server to include comments for expected functionality. Updated makefile to remove files ending in ~.

---
+++ assignment1/client1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public static void main(String[] args){
+/*	Input: 	<server ip address> <port number client1> <client1 password>
+ *		<file containing client1's RSA private exponent and modulus>
+ *		<file containing client2's RSA public exponent and modulus> 
+ *		<file name>*/
+
+	//Encrypt file with AES in CBC
+	//Hash plaintext with SHA-256
+	//Encrypt hash with RSA (private key)
+	//Send Encrypted data and signature to the server
+	//Encrypt password with client2 RSA key
+	//Send encrypted password to client 2 via server
+	//Disconnect from server after sending password, file, and signature 
+}
---
+++ assignment1/server.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public static void main(String[] args){
+/*	Input:	<port number for client1> <port number for client2> <mode> */
+
+	//Pass password from client 1 to client 2
+	//If trusted mode
+	//	send file unmodified and signature received from client 1 to 2
+	//If untrusted mode
+	//	replace encrypted file with file called "s-data" located in same directory as server and send the signature to client 2.
+}
