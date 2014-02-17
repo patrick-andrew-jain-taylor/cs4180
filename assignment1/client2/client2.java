fee4349fb8e0d24c8cf78d6b613426b46a46b783
Patrick Taylor <trickyconverse@gmail.com>
2014-02-17 17:13:25 -0500

    Directories made for client1, client2, and server, and all files relevant to these folders were moved.

---
+++ assignment1/client2/client2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public static void main(String[] args){
+/*	Input: 	<server ip address> <port number client2> 
+ *		<file containing client1's RSA private exponent and modulus>
+ *		<file containing client2's RSA public exponent and modulus> 
+ *		*/
+
+	//Check for invalid/garbage/missing input
+	//Receive encrypted file and signature from server
+	//Decrypt file and Verify Signature
+	//Write result to stdout
+	//Write unencrypted file received from server to disk in same directory as client 2 executable
+	//Name file "data" (no extension)
+	//Disconnect from server after receiving password, file, and signature
+}
