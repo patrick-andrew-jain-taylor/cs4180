82909323c7fb2a004c53603f891b382df815765f
Patrick Taylor <trickyconverse@gmail.com>
2014-02-17 18:26:50 -0500

    Client1.java fully debugged for input testing, next step will be encryption.

--- assignment1/client1/client1.java
+++ assignment1/client1/client1.java
@@ -2,14 +2,14 @@ import java.io.*;
 import java.lang.*;
 	
 public class client1{
-	private void argLength(String[] args){//checks for insufficient input size
+	private static void argLength(String[] args){//checks for insufficient input size
 		if (args.length != 6){ 
 			System.out.println("Input: <server ip address> <port number client1> <client1 password> < file containing client1's RSA	private exponent and modulus> <file containing client2's RSA public exponent and modulus> <file	name>");
 			System.out.println("client1 requires 6 input parameters.");
 			System.exit(0);
 		}
 	}
-	private String serverIpTest(String ipAddress){//checks for an valid server IP
+	private static String serverIpTest(String ipAddress){//checks for an valid server IP
 		if (ipAddress.matches("^.[0-9]{1,3}\\..[0-9]{1,3}\\..[0-9]{1,3}\\..[0-9]{1,3}") != true){
 			System.out.println("Input: <server ip address> <port number client1> <client1 password> < file containing client1's RSA private exponent and modulus> <file containing client2's RSA public exponent and modulus> <file name>");
 			System.out.println("<server ip address>: Please input a valid IPv4 server address ([0-255].[0-255].[0-255].[0-255]).");
@@ -17,17 +17,18 @@ public class client1{
 		}
 		return ipAddress;
 	}
-	private int portTest(String port) throws NumberFormatException{ //checks for a valid integer for client 1 port number
+	private static int portTest(String port) throws NumberFormatException{ //checks for a valid integer for client 1 port number
 		try {
 			int serverPort = Integer.parseInt(port);
+			return serverPort;
 		} catch (NumberFormatException e){
 			System.out.println("Input: <server ip address> <port number client1> <client1 password> < file containing client1's RSA private exponent and modulus> <file containing client2's RSA public exponent and modulus> <file name>");
 			System.out.println("<port number client1>: Please input a valid integer.");
 			System.exit(0);
 		}
-		return serverPort;
+		return 0;
 	}
-	private String passwordTest(String password){
+	private static String passwordTest(String password){
 		if (password.length() != 16){//checks for a 16 character password
 			System.out.println("Input: <server ip address> <port number client1> <client1 password> < file containing client1's RSA private exponent and modulus> <file containing client2's RSA public exponent and modulus> <file name>");
 			System.out.println("<client1 password>: Please input a password of exactly 16 characters.");
@@ -41,8 +42,8 @@ public class client1{
 		}
 		return password;
 	}
-	private File fileTest(String fileName, String input){
-		File file = new File(args[3]);
+	private static File fileTest(String fileName, String input){
+		File file = new File(fileName);
 		if (!file.exists()){
 			System.out.println("Input: <server ip address> <port number client1> <client1 password> < file containing client1.s RSA private exponent and modulus> <file containing client2.s RSA public exponent and modulus> <file name>");
 			System.out.println(input + ": Input a valid file");
@@ -81,7 +82,5 @@ public class client1{
 		//Send encrypted password to client 2 via server
 		//Disconnect from server after sending password, file, and signature
 	}
-}
-
 }
 
