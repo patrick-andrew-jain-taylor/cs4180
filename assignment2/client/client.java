6388c0fc248fffc899b8287f21ac57c91633bae6
Patrick Taylor <trickyconverse@gmail.com>
2014-04-03 13:42:26 -0400

    Socket established, working on get/put functionality

---
+++ assignment2/client/client.java
@@ -0,0 +1,62 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+
+public class client{
+
+	//clientSocket: establishes SSL socket from client to server
+	public static SSLSocket clientSocket(String serverIP, int serverPort) throws Exception{ 
+		//create SSL Socket Factory
+		SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+		//create SSL Socket to server
+		//set system properties
+		System.setProperty("javax.net.ssl.keyStore", "client.keystore");
+		System.setProperty("javax.net.ssl.keyStorePassword", "client");
+		//initate connection to the server
+		SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket(serverIP, serverPort);
+		return sslSocket;
+	}
+	//acceptIn: to be printed out for all invalid user input
+	public static void acceptIn(){
+		System.out.println("Acceptable inputs:");
+		System.out.println("get [<path>/filename]");
+		System.out.println("put [<path>/filename]");
+		System.out.println("exit");
+	}
+	//getPut: checks for valid GET/PUT command
+	public static void getPut(String input){
+		String[] inputSplit = input.split("[ ]+"); //splits string into array of space-delimited strings
+		//check for proper length
+		if (inputSplit.length != 2) acceptIn(); //improper input size
+		//check for get or put
+		if (inputSplit[0].equals("get"));
+		else if (inputSplit[0].equals("put"));
+		else acceptIn(); //not get or put (or exit by extension)
+	}
+	public static void userInput(SSLSocket sslSocket) throws IOException{
+		String input = "start";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true){ //loop indefinitely
+			System.out.print("> "); //to be displayed on the terminal
+			//take input from the terminal
+			input = br.readLine();
+			if (input.equals("exit")) break; //time to close the socket
+			//if beyond this point, function is either GET or PUT and string needs to be parsed
+			getPut(input);
+		}
+		br.close();
+	}
+	public static void main(String[] args){
+		String serverIP = "128.59.15.30"; //server IP
+		int serverPort = 9955; //server Port
+		//create socket to server
+		try{
+			SSLSocket sslSocket = clientSocket(serverIP, serverPort);
+			//take input from user
+			userInput(sslSocket);
+			sslSocket.close(); //close the socket
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+}
