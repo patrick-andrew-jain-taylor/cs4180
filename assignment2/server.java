71b91ae8685f5682c69004e0352b7e4e87380645
Patrick Taylor <trickyconverse@gmail.com>
2014-04-07 00:02:31 -0400

    Socketing is completely written, now need to debug get/put commands .

--- assignment2/server.java
+++ assignment2/server.java
@@ -7,17 +7,56 @@ import java.io.*;
 import javax.net.ssl.*;
 
 public class server{
-	public static SSLServerSocket serverSocket(int serverPort) throws Exception{
-		//set system properties
-		System.setProperty("javax.net.ssl.keyStore", "server.keystore");
-		System.setProperty("javax.net.ssl.keyStorePassword", "server");
-		SSLServerSocketFactory sslServerSocketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault(); //use default settings for server socket factory
-		//create SSL Socket for client
-		SSLServerSocket sslServerSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(serverPort); //create socket on serverPort
-		//require socket to have client authentication
-		sslServerSocket.setNeedClientAuth(true);
-		return sslServerSocket;
+	//getPut: checks for valid GET/PUT command
+	public static int getPut(String inputSplit[]){
+		//check for proper length
+		if (inputSplit.length != 2) return -1; //improper input size	
+		//check for get or put
+		if (inputSplit[0].equals("get")) return 1;
+		else if (inputSplit[0].equals("put")) return 0;
+		else return -1; //not get or put (or exit by extension)
 	}
+	//get: Sends file to client
+	public static int get(BufferedOutputStream out, String command, String file){
+		try{
+			FileInputStream get = new FileInputStream(file);
+			BufferedInputStream getBuf = new BufferedInputStream(get);
+			byte[] data = new byte[512];
+			int count = 0;
+			try{
+				while((count = getBuf.read(data, 0, data.length)) > 0)
+					out.write(data, 0, count);
+				getBuf.close();
+				return 0;
+			} catch (IOException e){
+				return serverError.WriteError();
+			}
+		} catch (FileNotFoundException e){
+			return serverError.FileError();
+		}
+	}
+	//put: Places file in server directory
+	public static int put(BufferedInputStream in, String command, String file){
+		try{
+			FileOutputStream put = new FileOutputStream(file);
+			BufferedOutputStream putBuf = new BufferedOutputStream(put);
+			byte[] data = new byte[512];
+			int count = 0;
+			try{
+				while((count = in.read(data, 0, data.length)) > 0) //read in buffered file from client
+					putBuf.write(data, 0, count);
+				putBuf.flush();
+				putBuf.close();
+				return 0;
+			} catch (IOException e){
+				return serverError.WriteError();
+			}
+		} catch (FileNotFoundException e){
+			return serverError.FileError();
+		}
+	}
+
+
 	public static void main(String[] args){
 		int serverPort = 9955; //server Port
 		try{
@@ -43,12 +82,37 @@ public class server{
 			serverSocket.setEnabledProtocols(new String[]{"TLSv1"});
 			SSLSocket sslSocket = (SSLSocket) serverSocket.accept();
 			//open a buffered stream on the socket
-			BufferedInputStream in = new BufferedInputStream(sslSocket.getInputStream());
-			in.read();
-			in.close(); //close input stream
+			BufferedOutputStream outBuf = new BufferedOutputStream(sslSocket.getOutputStream());
+			BufferedInputStream inBuf = new BufferedInputStream(sslSocket.getInputStream());
+			while(true){//loop indefinitely
+				byte[] command = new byte[512];
+				inBuf.read(command, 0, command.length);
+				String commandParse = new String(command, "UTF-8");
+				String[] commandSplit = commandParse.split("[ ]+");
+				if (commandSplit[0].equals("exit")) break; //time to close the socket
+				int getPut = getPut(commandSplit);
+				if (getPut < 0);
+				else{
+					if (getPut > 0) get (outBuf, commandParse, commandSplit[1]);
+					else put(inBuf, commandParse, commandSplit[1]);
+				}
+			}
+			inBuf.close(); //close input stream
+			outBuf.close(); //close output stream
 			sslSocket.close(); //close socket
 		} catch (Exception e){
 			e.printStackTrace();
 		}
 	}
+	private static class serverError{
+		public static int WriteError(){
+		System.out.println("Writing not allowed");
+		return -1;
+		}
+		public static int FileError(){
+		System.out.println("File not found");
+		return -1;
+		}
+	}
+
 }
