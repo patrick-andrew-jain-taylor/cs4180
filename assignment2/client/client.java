97e36c79ee5fa33f2baf9c7e41be5789233ccdf5
Patrick Taylor <trickyconverse@gmail.com>
2014-04-08 00:08:17 -0400

    Client fully commented.

--- assignment2/client/client.java
+++ assignment2/client/client.java
@@ -5,6 +5,10 @@ import java.io.*;
 import javax.net.ssl.*;
 import javax.net.ssl.SSLContext;
 import java.security.cert.Certificate;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class client{
 	//getPut: checks for valid GET/PUT command
@@ -18,57 +22,56 @@ public class client{
 	}
 	//get: Retrieves file from server
 	public static int get(BufferedInputStream in, String command, String file){
-	 	File fileOut = new File(file);
+	 	File fileOut = new File(file); //create new file to be output to
 		try{
-			FileOutputStream get = new FileOutputStream(fileOut);
-			BufferedOutputStream getBuf = new BufferedOutputStream(get);
-			byte[] data = new byte[512];
+			FileOutputStream get = new FileOutputStream(fileOut); //open file output stream
+			BufferedOutputStream getBuf = new BufferedOutputStream(get); //buffer file output
+			byte[] data = new byte[512]; //will read in file data
 			int count = 0;
 			try{
 				while((count = in.read(data, 0, data.length)) > 0){ //read in buffered file from server
-					System.out.println(count);
-					String dataParse = new String(data, 0, count);
-					String fail = "File not found";
-					if (dataParse.equals(fail)) {
-						System.out.println(fail);
+					String dataParse = new String(data, 0, count); //create string from data
+					String fail = "File not found"; //to be compared to
+					if (dataParse.equals(fail)) { //make comparison
+						System.out.println(fail); //file not found
 						break;
 					}
-					getBuf.write(data, 0, count);
-					if (count != data.length) break;
+					getBuf.write(data, 0, count); //write data to file
+					if (count != data.length) break; //break loop if EOF
 				}
-				getBuf.flush();
-				getBuf.close();
+				getBuf.flush(); //flush file buffer
+				getBuf.close(); //close file buffer
 				return 0;
 			} catch (IOException e){
-				return clientError.WriteError();
+				return clientError.WriteError(); //writing failed
 			}
 		} catch (FileNotFoundException e){
-			return clientError.FileError();
-		}
+			return clientError.FileError(); //file not found
+		} 
 	}
 	//put: Places file on server
 	public static int put(BufferedOutputStream out, String command, String file){
 		try{
 			out.write(command.getBytes()); //send command to server
-			out.flush();
-			FileInputStream put = new FileInputStream(file);
-			BufferedInputStream putBuf = new BufferedInputStream(put);
-			byte[] data = new byte[512];
-			int count = 0;
+			out.flush(); //flush output stream
+			FileInputStream put = new FileInputStream(file); //generate new file input stream
+			BufferedInputStream putBuf = new BufferedInputStream(put); //buffer the input stream
+			byte[] data = new byte[512]; //will store file data
+			int count = 0; //increments when data is read
 			try{
 				while((count = putBuf.read(data, 0, data.length)) > 0){ //read in buffered file from client
-					out.write(data, 0, count);
+					out.write(data, 0, count); //write file to server
 				}
-				out.flush();
-				putBuf.close();
-				return 0;
+				out.flush(); //flush output buffer
+				putBuf.close(); //close file buffer
+				return 0; //successful write
 			} catch (IOException e){
-				return clientError.WriteError();
+				return clientError.WriteError(); //unsuccessful write
 			}
 		} catch (FileNotFoundException e){
-			return clientError.FileError();
+			return clientError.FileError(); //file not found
 		}  catch (IOException e){
-			return clientError.WriteError();
+			return clientError.WriteError(); //error with writing to server
 		}
 	}
 	//acceptIn: to be printed out for all invalid user input
@@ -78,77 +81,132 @@ public class client{
 		System.out.println("put [<path>/filename]");
 		System.out.println("exit");
 	}
-	public static void userInput(SSLSocket sslSocket) throws IOException{
-		String input = "start";
-		OutputStream out = sslSocket.getOutputStream();
-		BufferedOutputStream outBuf = new BufferedOutputStream(out);
-		InputStream in = sslSocket.getInputStream();
-		BufferedInputStream inBuf = new BufferedInputStream(in);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (true){ //loop indefinitely
-			System.out.print("> "); //to be displayed on the terminal
-			//take input from the terminal
-			input = br.readLine();
-			String[] inputSplit = input.split("[ ]+"); //splits string into array of space-delimited strings
-			if (inputSplit[0].equals("exit")){ //time to close the socket
-				outBuf.write(input.getBytes());
-				break;
-			}
-			//if beyond this point, function is either GET or PUT and string needs to be parsed
-			int getPut = getPut(inputSplit);
-			if(getPut < 0) acceptIn(); //invalid input
-			else {
-				if (getPut > 0) {
-					outBuf.write(input.getBytes()); //send command to server
-					outBuf.flush();
-					get(inBuf, input, inputSplit[1]); //get
-				}
-				else put(outBuf, input, inputSplit[1]); //put
-			}
+	//hostAvailabilityCheck: instantiates socket to connect to server if still available
+	public static boolean hostAvailabilityCheck(String serverIP, int serverPort){
+		try {
+			Socket s = new Socket(serverIP, serverPort); //instantiate socket
+			return true;
+		} catch (IOException e){
+			System.out.println("Server closed.");
 		}
+		return false;
+	}
+	public static void userInput(SSLSocket sslSocket, String serverIP, int serverPort) throws IOException{
+		//input: used for terminal input
+		String input = "start";
+		//create buffered streams for the socket
+		OutputStream out = sslSocket.getOutputStream(); //the socket output stream
+		BufferedOutputStream outBuf = new BufferedOutputStream(out); //buffered socket output stream
+		InputStream in = sslSocket.getInputStream(); //socket input stream
+		BufferedInputStream inBuf = new BufferedInputStream(in); //buffered socket input stream
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //terminal reader
+		System.out.print("> "); //to be displayed in the terminal
+		while (hostAvailabilityCheck(serverIP, serverPort)){ //close if socket closes
+				//take input from the terminal
+				input = br.readLine();
+				//split input into strings
+				String[] inputSplit = input.split("[ ]+"); //splits string into array of space-delimited strings
+				//check if "exit" occurs
+				if (inputSplit[0].equals("exit")){ //time to close the socket
+					outBuf.write(input.getBytes()); //write "exit" to server
+					break;
+				}
+				//if beyond this point, function is either GET or PUT and string needs to be parsed
+				int getPut = getPut(inputSplit); //check if GET or PUT (or error)
+				if(getPut < 0) acceptIn(); //invalid input
+				else {
+					if (getPut > 0) { //GET
+						outBuf.write(input.getBytes()); //send command to server
+						outBuf.flush(); //flush output buffer
+						get(inBuf, input, inputSplit[1]); //get file from server
+					}
+					else put(outBuf, input, inputSplit[1]); //put file on server
+				}
+				//next terminal line
+				System.out.println();
+				System.out.print("> ");
+		}
+		//close buffers
 		br.close();
 		in.close();
 		out.close();
 	}
 	public static void main(String[] args){
-		String serverIP = "128.59.15.39"; //server IP
-		int serverPort = 9955; //server Port
+		clientError.argLength(args); //args length test
+		String serverIP = clientError.serverIpTest(args[0]); //server IP test
+		int serverPort = clientError.portTest(args[1]); //server Port test
 		//create socket to server
 		try{
 			//create SSLcontext
-			String keyStoreType = "JKS";
-			String keyStorePath = "client.keystore";
-			String keyStorePassword = "client";
+			String keyStoreType = "JKS"; //required type for instantiated keystore
+			String keyStorePath = "client.keystore"; //directory of keystore
+			String keyStorePassword = "client"; //password of keystore
+			//generate keystore
 			KeyStore keyStore = socket.keyStoreTLS(keyStoreType, keyStorePath, keyStorePassword);
+			//generate key manager factory
 			KeyManagerFactory keyManagerFactory = socket.keyManagerTLS(keyStore, keyStorePassword);
-			//truststore
-			String trustStorePath = "clienttrust.keystore";
-			String trustStorePassword = keyStorePassword;
+			//generate truststore
+			String trustStorePath = "clienttrust.keystore"; //path of trust store
+			String trustStorePassword = keyStorePassword; //password of trust store
 			KeyStore trustStore = socket.keyStoreTLS(keyStoreType, trustStorePath, trustStorePassword);
+			//generate trust manager factory
 			TrustManagerFactory trustManagerFactory = socket.trustManagerTLS(trustStore);
 			//bring it all together
+			//create SSLContext (required for TLS Socketing)
 			SSLContext sslContext =	
 				socket.SSLContextTLS(keyManagerFactory, trustManagerFactory, keyStorePassword);
+			//create SSL Socket Factory
 			SSLSocketFactory socketFactory = sslContext.getSocketFactory();
+			//create SSL Socket
 			SSLSocket sslSocket = (SSLSocket) socketFactory.createSocket(serverIP, serverPort);
+			//set TLS v1 as protocol
 			sslSocket.setEnabledProtocols(new String[]{"TLSv1"});
+			//initiate handshaking
 			sslSocket.startHandshake();
 			//take input from user
-			userInput(sslSocket);
-			sslSocket.close(); //close the socket
-		} catch (Exception e){
+			userInput(sslSocket, serverIP, serverPort);
+			//close the socket
+			sslSocket.close();
+		} catch (Exception e){ //check for any exception
 			e.printStackTrace();
 		}
 	}
 	private static class clientError{
-		public static int WriteError(){
-		System.out.println("Writing not allowed");
-		return -1;
+		public static int WriteError(){ //print if writing not allowed
+			System.out.println("Writing not allowed");
+			return -1;
 		}
-		public static int FileError(){
-		System.out.println("File not found");
-		return -1;
+		public static int FileError(){ //print if file not found
+			System.out.println("File not found");
+			return -1;
 		}
+		private static void argLength(String[] args){//checks for insufficient input size
+			if (args.length != 2){ 
+				System.out.println("Input: <server's ip or hostname> <port number>");
+				System.out.println("client requires 2 input parameters.");
+				System.exit(-1);
+			}
+		}
+		private static String serverIpTest(String ipAddress){//checks for an valid server IP
+			if (ipAddress.matches("^.[0-9]{1,3}\\..[0-9]{1,3}\\..[0-9]{1,3}\\..[0-9]{1,3}") != true){
+				System.out.println("Input: <server's ip or hostname> <port number>");
+				System.out.println("<server ip address>: Please input a valid IPv4 server address ([0-255].[0-255].[0-255].[0-255]).");
+				System.exit(-1);
+			}
+			return ipAddress;
+		}
+		private static int portTest(String port) throws NumberFormatException{ //checks for a valid integer for client 1 port number
+			try {
+				int serverPort = Integer.parseInt(port);
+				return serverPort;
+			} catch (NumberFormatException e){
+				System.out.println("Input: <server's ip or hostname> <port number>");
+				System.out.println("<port number>: Please input a valid integer.");
+				System.exit(-1);
+			}
+			return 0;
+		}
+
 	}
 
 }
